a732030042b5db145de8572de9baf6892603aeee
Satyam <satyamx64@gmail.com>
2020-06-24 19:44:29 +0530

    Final Minor UI Fixes

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 import 'package:youtubeclone/Models/colors.dart';
 import 'package:youtubeclone/Models/textstyles.dart';
@@ -17,7 +16,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Youtube Clone',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
@@ -32,24 +31,31 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
   @override
   Widget build(BuildContext context) {
-     // This trailing comma makes auto-formatting nicer for build methods.
-     return DefaultTabController(
-        length: 5,
-        child: new Scaffold(
-          appBar: AppBar(
-            actions: <Widget>[
-              Container(
-              child: Row(children: <Widget>[
-              Container(
-                width: 40,
-                child: Image(image: AssetImage("assets/youtube_logo.png"),),
+    // This trailing comma makes auto-formatting nicer for build methods.
+    return DefaultTabController(
+      length: 5,
+      child: new Scaffold(
+        appBar: AppBar(
+          actions: <Widget>[
+            Container(
+              child: Row(
+                children: <Widget>[
+                  Container(
+                    width: 40,
+                    child: Image(
+                      image: AssetImage("assets/youtube_logo.png"),
+                    ),
+                  ),
+                  Text(
+                    "YouTube",
+                    style: youtube,
+                  ),
+                ],
               ),
-              Text("YouTube", style: youtube,),
-              ],),),
-              Container(
+            ),
+            Container(
                 padding: EdgeInsets.only(left: 100, bottom: 15),
                 width: 260,
                 child: Container(
@@ -63,46 +69,50 @@ class _MyHomePageState extends State<MyHomePage> {
                       Container(
                         height: 30,
                         width: 30,
-                        child: CircleAvatar(backgroundImage: channel.profilePicture),
+                        child: CircleAvatar(
+                            backgroundImage: channel.profilePicture),
                       )
                     ],
                   ),
-                )
-                ),
-            ],
-            backgroundColor: tabBarColor,
-          ),
-          body: TabBarView(
-            children: [
-              new Container(
-                color: backgroundColor,
-                child: ListView(children: getVideos(false),),
-              ),
-              new Container(color: Colors.orange,),
-              new Container(
-                color: backgroundColor,
-                child: ListView(
-                  children: getVideos(true),
-                ),
-              ),
-              new Container(
-                color: Colors.red,
-              ),
-              new Container(
-                color: Colors.blue,
-              ),
-            ],
-          ),
-          bottomNavigationBar: new TabBar(
-            labelStyle: tabTextStyle,
-            tabs: tabList,
-            labelColor: tabBarSelectedIconsColor,
-            unselectedLabelColor: tabBarUnselectedIconsColor,
-            indicatorColor: Colors.transparent,
-          ),
+                )),
+          ],
           backgroundColor: tabBarColor,
         ),
-      );
+        body: TabBarView(
+          children: [
+            new Container(
+              color: backgroundColor,
+              child: ListView(
+                children: getVideos(false),
+              ),
+            ),
+            new Container(
+              color: Colors.orange,
+            ),
+            new Container(
+              color: backgroundColor,
+              child: ListView(
+                children: getVideos(true),
+              ),
+            ),
+            new Container(
+              color: Colors.red,
+            ),
+            new Container(
+              color: Colors.blue,
+            ),
+          ],
+        ),
+        bottomNavigationBar: new TabBar(
+          labelStyle: tabTextStyle,
+          tabs: tabList,
+          labelColor: tabBarSelectedIconsColor,
+          unselectedLabelColor: tabBarUnselectedIconsColor,
+          indicatorColor: Colors.transparent,
+        ),
+        backgroundColor: tabBarColor,
+      ),
+    );
   }
 
   List<Widget> getVideos(bool isSubscriptionPage) {
@@ -110,33 +120,35 @@ class _MyHomePageState extends State<MyHomePage> {
     List<Widget> cards = [];
     if (isSubscriptionPage) {
       cards.add(Container(
-      color: backgroundColor, height: 140,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: <Widget>[
-          Row(
-            children: <Widget>[
+        color: backgroundColor,
+        height: 140,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: <Widget>[
+            Row(children: <Widget>[
               ChannelAvatar(channel: channel),
-            ]
-          ),
-          Divider(color: tabBarUnselectedIconsColor,),
-          SuggestionsTab()
-        ],
-      ),
-      )
-    );
+            ]),
+            Divider(
+              color: tabBarUnselectedIconsColor,
+            ),
+            SuggestionsTab()
+          ],
+        ),
+      ));
     } else {
       cards.add(Container(
         padding: EdgeInsets.only(top: 5, bottom: 5),
-      color: backgroundColor, height: 50,
-      child: SuggestionsTab(),
-      )
-    );
+        color: backgroundColor,
+        height: 50,
+        child: SuggestionsTab(),
+      ));
     }
     for (Video video in videos) {
-      cards.add(VideoCard(video: video,));
+      cards.add(VideoCard(
+        video: video,
+      ));
     }
-    return cards;   
+    return cards;
   }
 
   List<String> videoTitles = [
@@ -147,42 +159,42 @@ class _MyHomePageState extends State<MyHomePage> {
     "Super Quick Python Porject Ideas For Beginners!"
   ];
 
-  Channel channel = new Channel("ChannelName", AssetImage("assets/profilepics/0.png"));
+  Channel channel =
+      new Channel("ChannelName", AssetImage("assets/profilepics/0.png"));
 
   List<Video> makeVideos() {
     List<Video> vids = [];
     for (int i = 0; i < 5; i++) {
       vids.add(new Video(
-        AssetImage("assets/thumbnails/" + i.toString() + ".jpg"),
-        120000,
-        DateTime.now().subtract(new Duration(days: 400)),
-        videoTitles[i],
-        channel
-      ));
+          AssetImage("assets/thumbnails/" + i.toString() + ".jpg"),
+          120000,
+          DateTime.now().subtract(new Duration(days: 400)),
+          videoTitles[i],
+          channel));
     }
     return vids;
   }
 
   List<Widget> tabList = [
-              Tab(
-                icon: new Icon(Icons.home),
-                text: "Home",
-              ),
-              Tab(
-                icon: new Icon(Icons.explore),
-                text: "Explore",
-              ),
-              Tab(
-                icon: new Icon(Icons.subscriptions),
-                text: "Subscriptions",
-              ),
-              Tab(
-                icon: new Icon(Icons.email),
-                text: "Inbox",
-              ),
-              Tab(
-                icon: new Icon(Icons.video_library),
-                text: "Library",
-              )
-            ];
+    Tab(
+      icon: new Icon(Icons.home),
+      text: "Home",
+    ),
+    Tab(
+      icon: new Icon(Icons.explore),
+      text: "Explore",
+    ),
+    Tab(
+      icon: new Icon(Icons.subscriptions),
+      text: "Subscriptions",
+    ),
+    Tab(
+      icon: new Icon(Icons.email),
+      text: "Inbox",
+    ),
+    Tab(
+      icon: new Icon(Icons.video_library),
+      text: "Library",
+    )
+  ];
 }
